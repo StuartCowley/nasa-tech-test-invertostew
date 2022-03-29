32fde2fbbb076878112587e42e1cf7888a723f26
Stuart Green <github.47a8z@aleeas.com>
2022-03-30 00:28:51 +0100

    refactor: wrap Search component in form, add submit button

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,22 @@ import { useState } from "react";
 import "../styles/Search.css";
 
 function Search() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <>
-      <input type="text" name="search" id="search" placeholder="Search" value="" />
+      <form className="search-form">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="search-form__input"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit" className="search-form__button">Search</button>
+      </form>
     </>
   );
 }
