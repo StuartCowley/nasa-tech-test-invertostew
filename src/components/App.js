5b128b2761f30b1e704706f98e6755680558ed71
Stuart Green <github.47a8z@aleeas.com>
2022-03-30 00:16:26 +0100

    feat: create Search component and test

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
+import Search from "./Search";
+
 import "../styles/App.css";
 
 function App() {
   return (
     <div className="nasa-app">
-      <p>Hello World!</p>
+      <img
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="Nasa"
+        className="nasa-app__logo"
+      />
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "../styles/Search.css";
+
+function Search() {
+  return (
+    <>
+      <input type="text" name="search" id="search" placeholder="Search" value="" />
+    </>
+  );
+}
+
+export default Search;
