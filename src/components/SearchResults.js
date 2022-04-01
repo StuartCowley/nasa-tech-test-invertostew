a53dee1e147eba28d6fa2a5282f38cea24a6fa69
Stuart Green <github.47a8z@aleeas.com>
2022-04-01 20:11:48 +0100

    style: add styling to project

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,16 +1,20 @@
 import PropTypes from "prop-types";
 
+import "../styles/SearchResults.css";
+
 function SearchResults({ searchResults }) {
   if (!searchResults.length) {
-    return <p>No search results found.</p>;
+    return <h3 className="search-results">No search results found.</h3>;
   }
 
   return (
     <>
-      <p>Search Results</p>
-      {searchResults.map((src) => (
-        <img key={src} src={src} alt="" />
-      ))}
+      <h3 className="search-results">Search Results</h3>
+      <div className="search-results__container">
+        {searchResults.map((src) => (
+          <img key={src} src={src} alt="" className="search-results-img" />
+        ))}
+      </div>
     </>
   );
 }
